fb7ba6915479afcb5ec8e07643d72a02ccc8dce3
Justin Weiskittel <justinweiskittel@gmail.com>
2018-12-13 12:42:40 -0500

    title and label update

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,14 +9,16 @@ import { YelpService } from '../services/yelp.service';
 })
 export class SearchComponent implements OnInit {
 
-  states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-  restaurants: any
-  i: number
-  rating: number
+  states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',
+   'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+   'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
+  restaurants: any;
+  i: number;
+  rating: number;
 
-  city = new FormControl()
-  state = new FormControl()
-  price = new FormControl()
+  city = new FormControl();
+  state = new FormControl();
+  price = new FormControl();
 
   constructor(private yelp: YelpService) { }
 
@@ -26,9 +28,9 @@ export class SearchComponent implements OnInit {
 
   search() {
     this.yelp.search(this.city.value, this.state.value, this.price.value).subscribe(data => {
-      this.restaurants = data
-      this.i = Math.floor(Math.random()*50)
-      this.rating = this.restaurants.businesses[this.i].rating
+      this.restaurants = data;
+      this.i = Math.floor(Math.random() * 50);
+      this.rating = this.restaurants.businesses[this.i].rating;
     });
   }
 
